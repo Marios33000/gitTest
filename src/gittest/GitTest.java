86c235d75e08af7aa9ec58e438a6a0a76bca3f6c
mario <marios291098@hotmail.com>
2019-11-27 20:07:36 +0200

    marios

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -15,7 +15,6 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Kante to pull remalia");
     }
-    
 }
